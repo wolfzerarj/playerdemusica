3c0d5535f31e872e36a200865eeb2a444707a0ba
Igor Xisto <igorxisto12@gmail.com>
2021-12-08 15:48:32 -0300

    Projeto 1

---
+++ script.js
@@ -0,0 +1,74 @@
+let musicas = [
+    {titulo: ' Pisciana (Prod.Simøn)', artista:'Gabziim x Simøn x Agostta x Ravena', src:'musicas/Gabziim x Simøn x Agostta x Ravena - Pisciana (Prod.Simøn) .mp3', img:'img/1.png'},
+
+    {titulo: ' pixote Mix', artista:'pixote', src:'musicas/Pixote.mp3', img:'img/2.jpg'},
+
+    {titulo: 'Até você voltar ', artista:'Henrique & Juliano', src:'musicas/Henrique e Juliano - Até Você Voltar.mp3', img:'img/3.jpg'}
+
+]
+
+let musica = document.querySelector('audio');
+let indexMusica= 0;
+let duracaoMusica = document.querySelector('.fim');
+let imagem = document.querySelector('img');
+let nomeMusica = document.querySelector('.descrição h2');
+let nomeArtista = document.querySelector('.descrição i');
+
+renderizarMusica(indexMusica);
+
+//eventos do script
+document.querySelector('.botao-play').addEventListener('click', tocarMusica);
+document.querySelector('.botao-pause').addEventListener('click', pausarMusica);
+musica.addEventListener('timeupdate', atualizarBarra);
+document.querySelector('.anterior').addEventListener('click', () => {
+    indexMusica--;
+    if (indexMusica < 0) {
+        indexMusica = 2;
+    }
+    renderizarMusica(indexMusica);
+});
+document.querySelector('.proxima').addEventListener('click', () => {
+    indexMusica++;
+    if (indexMusica > 2) {
+        indexMusica = 0;
+    }
+    renderizarMusica(indexMusica);
+})
+
+//funções
+
+function renderizarMusica(index){
+    musica.setAttribute('src', musicas[index].src);
+    musica.addEventListener('loadeddata', () => {
+        nomeMusica.textContent = musicas[index].titulo;
+        nomeArtista.textContent = musicas[index].artista;
+        imagem.src = musicas[index].img;
+        duracaoMusica.textContent =segundosParaMinutos(Math.floor(musica.duration));
+
+    });
+}
+
+function tocarMusica(){
+    musica.play();
+    document.querySelector('.botao-pause').style.display='block';
+    document.querySelector('.botao-play').style.display='none';
+}
+function pausarMusica(){
+    musica.pause();
+    document.querySelector('.botao-pause').style.display='none';
+    document.querySelector('.botao-play').style.display='block';
+}
+function atualizarBarra(){
+    let barra = document.querySelector('progress');
+    barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%';
+    let tempoDecorrido = document.querySelector('.inicio');
+    tempoDecorrido.textContent = segundosParaMinutos(Math.floor(musica.currentTime));
+
+}function segundosParaMinutos(segundos){
+    let campoMinutos = Math.floor(segundos/ 60);
+    let campoSegundos = segundos % 60;
+    if(campoSegundos < 10){
+        campoSegundos = '0' + campoSegundos;
+    }
+    return campoMinutos + ':' + campoSegundos;
+}
