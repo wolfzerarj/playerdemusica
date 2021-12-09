39aba0aef18e3220f7d8ea596373edf45403dd20
Igor Xisto <igorxisto12@gmail.com>
2021-12-09 12:48:29 -0300

    Atualização 2

--- script.js
+++ script.js
@@ -1,35 +1,55 @@
-let musicas = [
-    {titulo: ' Pisciana (Prod.Simøn)', artista:'Gabziim x Simøn x Agostta x Ravena', src:'musicas/Gabziim x Simøn x Agostta x Ravena - Pisciana (Prod.Simøn) .mp3', img:'img/1.png'},
+
+var musicas = [
+    {titulo: 'Ponto de Paz ', artista:'Theô', src:'musicas/PontodePaz.mp3', img:'img/7.png'},
 
     {titulo: ' pixote Mix', artista:'pixote', src:'musicas/Pixote.mp3', img:'img/2.jpg'},
 
-    {titulo: 'Até você voltar ', artista:'Henrique & Juliano', src:'musicas/Henrique e Juliano - Até Você Voltar.mp3', img:'img/3.jpg'}
+    {titulo: 'meia noite e pouca', artista:'Lowz', src:'musicas/Lowz - Meia noite e pouca (prod. Simøn).mp3', img:'img/5.jpg'},
 
-]
+    {titulo: 'Até você voltar ', artista:'Henrique & juliano', src:'musicas/Henrique e Juliano - Até Você Voltar.mp3', img:'img/3.jpg'},
 
-let musica = document.querySelector('audio');
-let indexMusica= 0;
-let duracaoMusica = document.querySelector('.fim');
-let imagem = document.querySelector('img');
-let nomeMusica = document.querySelector('.descrição h2');
-let nomeArtista = document.querySelector('.descrição i');
+    {titulo: 'Pisciana (Prod.Simøn)', artista:'Gabziim x Simøn x Agostta x Ravena', src:'musicas/Gabziim x Simøn x Agostta x Ravena - Pisciana (Prod.Simøn).mp3', img:'img/1.png'},
+
+
+    {titulo:'Cachecol (Prod.MarcusMaia)', artista:'K a m a i t a c h i, Sanza ,Prod.MarcusMaia', src:'musicas/K a m a i t a c h i ft. Sanza - Cachecol (Prod.MarcusMaia).mp3', img:'img/cachecol.jpg'},
+
+    {titulo:'Café das 6', artista:'K a m a i t a c h i', src:'musicas/K a m a i t a c h i - Café das 6.mp3', img:'img/cafédas6.jpg'},
+
+    {titulo:'Carrinho de madeira', artista:'K a m a i t a c h i', src:'musicas/K a m a i t a c h i - carrinho de madeira.mp3', img:'img/carrinhodemadeira.jpg'},
+
+    {titulo:'Cabelos Arco-íris', artista:'K a m a i t a c h i', src:'musicas/k a m a i t a c h i - Cabelos Arco-íris.mp3', img:'img/cabelosarcoiris.jpg'},
+];
+
+var musica = document.querySelector('audio');
+var indexMusica= 0;
+
+
+var duracaoMusica = document.querySelector('.fim');
+var imagem = document.querySelector('img');
+var nomeMusica = document.querySelector('.descrição h2');
+var nomeArtista = document.querySelector('.descrição i');
 
 renderizarMusica(indexMusica);
 
 //eventos do script
 document.querySelector('.botao-play').addEventListener('click', tocarMusica);
+
 document.querySelector('.botao-pause').addEventListener('click', pausarMusica);
+
+
+
 musica.addEventListener('timeupdate', atualizarBarra);
+
 document.querySelector('.anterior').addEventListener('click', () => {
     indexMusica--;
     if (indexMusica < 0) {
-        indexMusica = 2;
+        indexMusica = 9;
     }
     renderizarMusica(indexMusica);
 });
 document.querySelector('.proxima').addEventListener('click', () => {
     indexMusica++;
-    if (indexMusica > 2) {
+    if (indexMusica > 9) {
         indexMusica = 0;
     }
     renderizarMusica(indexMusica);
@@ -59,14 +79,16 @@ function pausarMusica(){
     document.querySelector('.botao-play').style.display='block';
 }
 function atualizarBarra(){
-    let barra = document.querySelector('progress');
+    var barra = document.querySelector('progress');
     barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%';
-    let tempoDecorrido = document.querySelector('.inicio');
+    var tempoDecorrido = document.querySelector('.inicio');
     tempoDecorrido.textContent = segundosParaMinutos(Math.floor(musica.currentTime));
+}
 
-}function segundosParaMinutos(segundos){
-    let campoMinutos = Math.floor(segundos/ 60);
-    let campoSegundos = segundos % 60;
+
+function segundosParaMinutos(segundos){
+    var campoMinutos = Math.floor(segundos/ 60);
+    var campoSegundos = segundos % 60;
     if(campoSegundos < 10){
         campoSegundos = '0' + campoSegundos;
     }
